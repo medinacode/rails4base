e27933f0b7312304f8118878ec7ce317b181dd3f
Eric Smith <medinacode@gmail.com>
2014-10-31 21:17:19 -0400

    Stylesheets, layout, and default init

---
+++ app/assets/javascripts/layout.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.validator.addMethod(
+        "regex",
+        function(value, element, regexp) {
+            var re = new RegExp(regexp);
+            return this.optional(element) || re.test(value);
+        },
+        "Please check your input."
+    );
+
+    $('form.validate').validate({
+        showErrors: function(errorMap, errorList) {
+            $.each( this.successList , function(index, value) {
+                $(value).popover('hide');
+            });
+            $.each( errorList , function(index, value) {
+                var _popover = $(value.element).popover({
+                    trigger: 'manual',
+                    placement: 'right',
+                    template: '<div class="popover"><div class="arrow"></div><div class="popover-inner"><div class="popover-content"><p></p></div></div></div>',
+                    html: true
+                });
+                _popover.data('bs.popover').options.content = '<i class="fa fa-times"></i>' + value.message;
+                $(value.element).popover('show');
+            });
+        }
+    });
+
+})
--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,30 @@ Bundler.require(*Rails.groups)
 
 module Rails4base
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+      config.time_zone = 'Eastern Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+      # custom include paths
+      config.autoload_paths += %W["#{config.root}/app/validators"]
+
+      # don't use coffeescript in generators
+      config.generators.javascript_engine :js
+
+      config.generators do |g|
+          # don't create per-scaffold assets with generator
+          g.javascripts false
+          g.stylesheets false
+          # use custom generator files if they exist
+          g.templates.unshift File::expand_path('../../templates', __FILE__)
+      end
+
+      # don't log passwords
+      config.filter_parameters << :password
+
+      # bootstrap compatible form errors
+      config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+          html_tag
+      }
+
   end
 end
