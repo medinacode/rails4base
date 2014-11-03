0320a799033693d3ff173b284462a1b6a1f35417
Eric Smith <medinacode@gmail.com>
2014-11-02 21:23:21 -0500

    Added gems and configuration for rspec testing

--- config/application.rb
+++ config/application.rb
@@ -7,34 +7,43 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Rails4base
-  class Application < Rails::Application
+    class Application < Rails::Application
 
-      config.time_zone = 'Eastern Time (US & Canada)'
+        config.time_zone = 'Eastern Time (US & Canada)'
 
-      # custom include paths
-      config.autoload_paths += %W["#{config.root}/app/validators"]
-      config.autoload_paths += %W["#{config.root}/app/lib"]
+        # custom include paths
+        config.autoload_paths += %W["#{config.root}/app/validators"]
+        config.autoload_paths += %W["#{config.root}/app/lib"]
 
-      # don't use coffeescript in generators
-      config.generators.javascript_engine :js
+        # don't use coffeescript in generators
+        config.generators.javascript_engine :js
 
-      config.generators do |g|
-          # don't create per-scaffold assets with generator
-          g.javascripts false
-          g.stylesheets false
-          # use custom generator files if they exist
-          g.templates.unshift File::expand_path('../../templates', __FILE__)
-          g.template_engine :custom
-          g.fallbacks[:custom] = :erb
-      end
+        config.generators do |g|
+            # don't create per-scaffold assets with generator
+            g.javascripts false
+            g.stylesheets false
+            # use custom generator files if they exist
+            g.templates.unshift File::expand_path('../../templates', __FILE__)
+            g.template_engine :custom
+            g.fallbacks[:custom] = :erb
+            # rspec
+            g.test_framework :rspec,
+                fixtures: true,
+                view_specs: false,
+                helper_specs: false,
+                routing_specs: false,
+                controller_specs: true,
+                request_specs: false
+            g.fixture_replacement :factory_girl, dir: 'spec/factories'
+        end
 
-      # don't log passwords
-      config.filter_parameters << :password
+        # don't log passwords
+        config.filter_parameters << :password
 
-      # bootstrap compatible form errors
-      config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+        # bootstrap compatible form errors
+        config.action_view.field_error_proc = Proc.new { |html_tag, instance|
           html_tag
-      }
+        }
 
-  end
+    end
 end
